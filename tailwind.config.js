ee42d49bb4c4bd6c785df18169494df7d26acb24
Onyeka <oaribeana@gmail.com>
2021-11-18 10:41:38 +0100

    Initial commit for Capstone Project 2

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        header: ["Fraunces", "sans-serif"],
+        paragraph: ["Barlow", "sans-serif"],
+      },
+      colors: {
+        "soft-red": "#fe7867",
+        "bright-yellow": "#fad400",
+        "desaturated-cyan": "#25564b",
+        "dark-blue": "#19536b",
+        "moderate-cyan": "#458c7e",
+        "desaturated-blue": "#23303e",
+        "darker-grayish-blue": "#5a636c",
+        "dark-grayish-blue": "#818498",
+        "grayish-blue": "#a7abae",
+        "footer-body": "#82c9bb",
+      },
+      backgroundImage: {
+        "desktop-header": "url('./images/desktop/image-header.jpg')",
+        "desktop-transform": "url('./images/desktop/image-transform.jpg')",
+        "desktop-stand-out": "url('./images/desktop/image-stand-out.jpg')",
+        "desktop-photography": "url('./images/desktop/image-photography.jpg')",
+        "desktop-graphic-design":
+          "url('./images/desktop/image-graphic-design.jpg')",
+        "mobile-header": "url('./images/mobile/image-header.jpg')",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
